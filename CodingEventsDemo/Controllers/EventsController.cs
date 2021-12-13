e3294f97faf03c289cc4b1cec53877cb0fa43f81
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-12-13 17:08:05 -0600

    Finished exercises

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -10,7 +10,10 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
-        static private List<string> Events = new List<string>();
+        // change this List into a Dictionary
+        // keys: event names
+        // values: event descriptions
+        static private Dictionary<string, string> Events = new Dictionary<string, string>();
 
         // GET: /<controller>/
         public IActionResult Index()
@@ -30,9 +33,9 @@ namespace coding_events_practice.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name)
+        public IActionResult NewEvent(string name, string description)
         {
-            Events.Add(name);
+            Events.Add(name, description);
 
             return Redirect("/Events");
         }
