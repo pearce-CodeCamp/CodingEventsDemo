a2f81bbc78fd7e05c0fbb21011c1df59166de9e4
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-12-15 17:20:08 -0600

    Finished exercise

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -28,10 +28,33 @@ namespace coding_events_practice.Controllers
 
         [HttpPost]
         [Route("Events/Add")]
+        // used to have 2 parameters: name, description
         public IActionResult NewEvent(Event newEvent)
         {
             EventData.Add(newEvent);
       
+            return Redirect("/Events");
+        }
+
+        [HttpGet]
+        [Route("Events/Edit/{eventId}")]
+        public IActionResult Edit(int eventId)
+        {
+            // use eventId to select an Event via EventData and store that event
+            Event eventToEdit = EventData.GetById(eventId);
+            ViewBag.eventToEdit = eventToEdit;
+            ViewBag.title = $"Editing {eventToEdit.Name} (ID={eventToEdit.Id})";
+            return View();
+        }
+
+        // path: /Events/Edit
+        [HttpPost]
+        [Route("/Events/Edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            Event edittedEvent = EventData.GetById(eventId);
+            edittedEvent.Name = name;
+            edittedEvent.Description = description;
             return Redirect("/Events");
         }
 
--- CodingEventsDemo/Data/EventData.cs
+++ CodingEventsDemo/Data/EventData.cs
@@ -7,27 +7,42 @@ namespace CodingEventsDemo.Data
 {
     public class EventData
     {
+        // string key is event name
+        // string value is event description
+        //static private Dictionary<string, string> Events = new Dictionary<string, string>();
+
+        // int key is event id
+        // Event value is an instance of the Event class... and Event object
         static private Dictionary<int, Event> Events = new Dictionary<int, Event>();
 
         // GetAll
+        // returns a List-esque collection of all Event objects stored in the Events
+        // dictionary
         public static IEnumerable<Event> GetAll()
         {
             return Events.Values;
         }
 
         // Add
+        // adds a KeyValuePair to the Events dictionary
+        // the key is the id of the Event we are adding
+        // the value is the Event object itself
         public static void Add(Event newEvent)
         {
             Events.Add(newEvent.Id, newEvent);
         }
 
         // Remove
+        // takes in an id integer
+        // removes the Event from Events that has the given id
         public static void Remove(int id)
         {
             Events.Remove(id);
         }
 
         // GetById
+        // takes in an id integer
+        // returns the Event from Events that has the given id
         public static Event GetById(int id)
         {
             return Events[id];
